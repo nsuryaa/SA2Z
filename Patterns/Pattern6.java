55fabee40c8b753025d819fe32fb3ee7cd3b963d
nsuryaa <suryasedulous@gmail.com>
2024-01-21 16:37:29 +0530

    patterns

--- Patterns/Pattern6.java
+++ Patterns/Pattern6.java
@@ -1,3 +1,24 @@
-public class Pattern6 {
+
+import java.util.Scanner;
+
+class Pattern6 {
+    static void printpattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(); // Move this line outside the inner loop
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            printpattern(n);
+        }
+    }
 
 }
